c2d54b1803bc3e9e52b9504d07916891f8ecec86
Terfa John <148336112+block-chain7@users.noreply.github.com>
2023-10-20 10:07:57 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,117 +1,163 @@
-var messages_content = $('.messages-content');
-var greeting_intro = 0;
+// Define variables
+var messagesContent = $('.messages-content');
+var greetingIntro = 0;
 var response;
-var main_data;
+var mainData;
 
-$(document).ready(function() {
+// Document ready event
+$(document).ready(function () {
+    // Fetch page views
+    $.getJSON("https://api.countapi.xyz/hit/eni4sure.github.io/hngi-chatbot", function (view) {
+        $("#page_view_no").html(view.value);
+    });
 
-	$.getJSON("https://api.countapi.xyz/hit/eni4sure.github.io/hngi-chatbot", function(view) {
-		$("#page_view_no").html( view.value );
-	});
-
-	$.getJSON("https://disease.sh/v2/countries/NG", function(response) {
-		main_data = response;
-	});
-
+    // Fetch COVID-19 data
+    $.getJSON("https://disease.sh/v2/countries/NG", function (data) {
+        mainData = data;
+    });
 });
 
-$(window).load(function() {
-	messages_content.mCustomScrollbar();
-	setTimeout(function() {
-		output_message();
-	}, 100);
+// Window load event
+$(window).load(function () {
+    // Initialize custom scrollbar
+    messagesContent.mCustomScrollbar();
+    setTimeout(function () {
+        outputMessage();
+    }, 100);
 });
 
+// Function to update scrollbar
 function updateScrollbar() {
-	messages_content.mCustomScrollbar("update").mCustomScrollbar('scrollTo', 'bottom', {
-		scrollInertia: 10,
-		timeout: 0
-	});
+    messagesContent.mCustomScrollbar("update").mCustomScrollbar('scrollTo', 'bottom', {
+        scrollInertia: 10,
+        timeout: 0
+    });
 }
 
-function insert_my_message() {
-	msg = $('.message-input').val();
-	if ($.trim(msg) == '') {
-		return false;
-	}
-	$('<div class="message message-me">' + msg + '</div>').appendTo($('.mCSB_container')).addClass('new');
-	updateScrollbar();
-	setTimeout(function() {
-		output_message();
-	}, 1000 + (Math.random() * 20) * 100);
+// Function to insert user's message
+function insertMyMessage() {
+    var msg = $('.message-input').val();
+    if ($.trim(msg) === '') {
+        return false;
+    }
+    $('<div class="message message-me">' + msg + '</div>').appendTo($('.mCSB_container')).addClass('new');
+    updateScrollbar();
+    setTimeout(function () {
+        outputMessage();
+    }, 1000 + (Math.random() * 20) * 100);
 }
 
-// On submit message
-$('.message-submit').click(function() {
-	insert_my_message();
-});
-$(window).on('keydown', function(e) {
-	if (e.which == 13) {
-		insert_my_message();
-		return false;
-	}
+// Event handler for submit button
+$('.message-submit').click(function () {
+    insertMyMessage();
 });
 
-function output_message() {
-	$('<div class="message loading new"><div class="avatar"><img src="assets/img/me.png" /></div><span></span></div>').appendTo($('.mCSB_container'));
-	updateScrollbar();
+// Event handler for Enter key
+$(window).on('keydown', function (e) {
+    if (e.which === 13) {
+        insertMyMessage();
+        return false;
+    }
+});
 
-	response = null;
-	message_react_value = null;
-	message_react_value = $('.message-input').val();
-	$('.message-input').val(null);
+// Function to generate bot's response
+function outputMessage() {
+    $('<div class="message loading new"><div class="avatar"><img src="assets/img/me.png" /></div><span></span></div>').appendTo($('.mCSB_container'));
+    updateScrollbar();
+    response = null;
+    var messageReactValue = $('.message-input').val();
+    $('.message-input').val(null);
 
-	if ( greeting_intro == 0 ) {
+    if (greetingIntro === 0) {
+        response = '<b>Hi</b> there, I\'m <b>Eni4sure</b> and you are?';
+    }
 
-		response = '<b>Hi</b> there, I\'m <b>Eni4sure</b> and you are?';
-	}
+    if (greetingIntro === 1) {
+        response =
+            'Nice to meet you <br> So here\'s the thing, I work with codes <br><br>' +
+            'Here are your options: <br>' +
+            '<b>0</b>: for COVID-19 helplines <br>' +
+            '<b>1</b>: for COVID-19 updates <br>' +
+            '<b>2</b>: for COVID-19 symptoms <br>' +
+            '<b>3</b>: for COVID-19 prevention <br><br>' +
+            'I was made by: <a href="https://linkedin.com/in/eniola-osabiya" target="_blank">Eniola Osabiya</a>';
+    }
 
-	if ( greeting_intro == 1 ) {
+    if (greetingIntro > 1) {
+        if (messageReactValue === "0" || messageReactValue === "1" || messageReactValue === "2" || messageReactValue === "3") {
+            if (messageReactValue === "0") {
+                response =
+                    'COVID-19 official toll-free helpline <br> ================== <br>' +
+                    '<a href="tel:+23480097000010">+23480097000010</a> <br><br>' +
+                    'I was made by: <a href="https://linkedin.com/in/eniola-osabiya" target="_blank">Eniola Osabiya</a>';
+            }
 
-		response = 'Nice to meet you <br> So here\'s the thing I work with codes <br><br> Here they are: <br> <b>0</b>: for covid-19 helplines <br> <b>1</b>: for covid-19 updates <br> <b>2</b>: for covid-19 symptoms <br> <b>3</b>: for covid-19 prevention <br><br> I was made by: <a href="https://linkedin.com/in/eniola-osabiya" target="_blank">Eniola Osabiya</a>';
-	}
+            if (messageReactValue === "1") {
+                response =
+                    'Summary of COVID-19 in Nigeria <br><br>' +
+                    'New Confirmed: <b>' + mainData.todayCases + '</b> <br>' +
+                    'Total Confirmed: <b>' + mainData.cases + '</b> <br>' +
+                    'New Deaths: <b>' + mainData.todayDeaths + '</b> <br>' +
+                    'Total Deaths: <b>' + mainData.deaths + '</b> <br>' +
+                    'New Recoveries: <b>' + mainData.todayRecovered + '</b> <br>' +
+                    'Total Recovered: <b>' + mainData.recovered + '</b> <br><br>' +
+                    '<b>Stay Home, Stay Safe.</b> <br><br>' +
+                    'I was made by: <a href="https://linkedin.com/in/eniola-osabiya" target="_blank">Eniola Osabiya</a>';
+            }
 
-	if ( greeting_intro > 1) {
+            if (messageReactValue === "2") {
+                response =
+                    'Common symptoms of COVID-19 include: <br><br>' +
+                    '- Fever <br>' +
+                    '- Fatigue <br>' +
+                    '- Dry and consistent cough <br>' +
+                    '- Difficulty in breathing <br>' +
+                    '- Runny or stuffy nose <br>' +
+                    '- Muscle aches, body aches, or headaches <br><br>' +
+                    'If you are experiencing any of these, please call the COVID-19 toll-free number now! <br>' +
+                    '<a href="tel:+23480097000010">+23480097000010</a> <br><br>' +
+                    'I was made by: <a href="https://linkedin.com/in/eniola-osabiya" target="_blank">Eniola Osabiya</a>';
+            }
 
-		if ( message_react_value == "0" || message_react_value == "1" || message_react_value == "2" || message_react_value == "3" ) {
+            if (messageReactValue === "3") {
+                response =
+                    'Prevention tips for COVID-19: <br><br>' +
+                    '- <b>STAY</b> home as much as you can <br>' +
+                    '- <b>KEEP</b> a safe distance <br>' +
+                    '- <b>WASH</b> your hands often <br>' +
+                    '- <b>COVER</b> your cough <br>' +
+                    '- <b>FEEL SICK?</b> Call ahead <a href="tel:+23480097000010">+23480097000010</a> <br><br>' +
+                    'I was made by: <a href="https://linkedin.com/in/eniola-osabiya" target="_blank">Eniola Osabiya</a>';
+            }
+        } else {
+            response =
+                'I don\'t understand that! <br>' +
+                'As I mentioned before, I work with codes <br><br>' +
+                'Here are your options again: <br>' +
+                '<b>0</b>: for COVID-19 helplines <br>' +
+                '<b>1</b>: for COVID-19 updates <br>' +
+                '<b>2</b>: for COVID-19 symptoms <br>' +
+                '<b>3</b>: for COVID-19 prevention <br><br>' +
+                'I was made by: <a href="https://linkedin.com/in/eniola-osabiya" target="_blank">Eniola Osabiya</a>';
+        }
+    }
 
-			if ( message_react_value == "0") {
+    if (response === null) {
+        response =
+            'Nice to meet you <br> So here\'s the thing, I work with codes <br><br>' +
+            'Here are your options: <br>' +
+            '<b>0</b>: for COVID-19 helplines <br>' +
+            '<b>1</b>: for COVID-19 updates <br>' +
+            '<b>2</b>: for COVID-19 symptoms <br>' +
+            '<b>3</b>: for COVID-19 prevention <br><br>' +
+            'I was made by: <a href="https://linkedin.com/in/eniola-osabiya" target="_blank">Eniola Osabiya</a>';
+    }
 
-				response = ` Covid-19 official toll free helpline <br> ================== <br> <a href="tel:+23480097000010">+23480097000010</a> <br><br> I was made by: <a href="https://linkedin.com/in/eniola-osabiya" target="_blank">Eniola Osabiya</a> `;
-			}
+    greetingIntro++;
 
-			if ( message_react_value == "1") {
-
-				response = ` Here is the summary of Covid-19 in Nigeria <br><br> New Confirmed: <b>`+main_data.todayCases+`</b> <br> Total Confirmed: <b>`+main_data.cases+`</b> <br> New Death: <b>`+main_data.todayDeaths+`</b> <br> Total Death: <b>`+main_data.deaths+`</b> <br> New Recoveries: <b>`+main_data.todayRecovered+`</b> <br> Total Recovered: <b>`+main_data.recovered+`</b> <br><br> <b>Stay Home, Stay Safe.</b> <br><br> I was made by: <a href="https://linkedin.com/in/eniola-osabiya" target="_blank">Eniola Osabiya</a> `;
-			}
-
-			if ( message_react_value == "2") {
-
-				response = ` Some of the symptoms of the covid-19 disease are: <br><br> - Fever <br> - Fatigue <br> - Dry and consistent cough <br> - Difficulty in breathing <br> - Runny or Stuffy nose <br> - Muscle aches, body aches or headaches. <br><br> If you are experiencing any of these, please call the covid-19 toll free now ! <br> <a href="tel:+23480097000010">+23480097000010</a> <br><br> I was made by: <a href="https://linkedin.com/in/eniola-osabiya" target="_blank">Eniola Osabiya</a> `;
-			}
-
-			if ( message_react_value == "3") {
-
-				response = ` Some prevention tips for covid 19: <br><br> - <b>STAY</b> home as much as you can <br> - <b>KEEP</b> a safe distance <br> - <b>WASH</b> hands often <br> - <b>COVER</b> your cough <br> - <b>SICK?</b> Call ahead <a href="tel:+23480097000010">+23480097000010</a> <br><br> I was made by: <a href="https://linkedin.com/in/eniola-osabiya" target="_blank">Eniola Osabiya</a> `;
-			}
-
-		} else {
-
-			response = 'I don\'t understand that! <br> Like I said before, I work with codes <br><br> Here they are again: <br> <b>0</b>: for covid-19 helplines <br> <b>1</b>: for covid-19 updates <br> <b>2</b>: for covid-19 symptoms <br> <b>3</b>: for covid-19 prevention <br><br> I was made by: <a href="https://linkedin.com/in/eniola-osabiya" target="_blank">Eniola Osabiya</a>';
-		}
-	}
-
-	if ( response == null ) {
-
-		response = 'Nice to meet you <br> So here\'s the thing I work with codes <br><br> Here they are: <br> <b>0</b>: for covid-19 helplines <br> <b>1</b>: for covid-19 updates <br> <b>2</b>: for covid-19 symptoms <br> <b>3</b>: for covid-19 prevention <br><br> I was made by: <a href="https://linkedin.com/in/eniola-osabiya" target="_blank">Eniola Osabiya</a>';
-	}
-
-	greeting_intro++;
-
-	setTimeout(function() {
-		$('.message.loading').remove();
-		$('<div class="message new"><div class="avatar"><img src="assets/img/me.png" /></div>' + response + '</div>').appendTo($('.mCSB_container')).addClass('new');
-		updateScrollbar();
-	}, 1000 + (Math.random() * 20) * 100);
-
+    setTimeout(function () {
+        $('.message.loading').remove();
+        $('<div class="message new"><div class="avatar"><img src="assets/img/me.png" /></div>' + response + '</div>').appendTo($('.mCSB_container')).addClass('new');
+        updateScrollbar();
+    }, 1000 + (Math.random() * 20) * 100);
 }
